008b7cf9fb65e6c8b2e21500681920557b801a8e
Adeonir Kohl <adeonir@gmail.com>
2023-09-24 15:31:25 -0300

    add custom colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        coffee: {
+          50: '#fbf9eb',
+          100: '#f7efca',
+          200: '#f0dd98',
+          300: '#e7c55d',
+          400: '#deab31',
+          500: '#c08b21',
+          600: '#b2741c',
+          700: '#8e551a',
+          800: '#77441c',
+          900: '#653a1e',
+          950: '#522914',
+        },
+      },
+    },
   },
   plugins: [],
 }
