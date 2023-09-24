91a30b6e1193c923ee5a12636cac1b90585e0bdb
Adeonir Kohl <adeonir@gmail.com>
2023-09-24 15:52:53 -0300

    install phosphor icons

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
+import { Coffee } from 'phosphor-react'
+
 export function App() {
-  return <h1 className="text-3xl font-bold">Coffee Delivery</h1>
+  return (
+    <h1 className="text-3xl font-bold flex items-center gap-2">
+      <Coffee weight="fill" /> Coffee Delivery
+    </h1>
+  )
 }
