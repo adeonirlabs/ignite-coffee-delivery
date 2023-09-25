7e96a4387513f75b98f8a99b20e805c49f002848
Adeonir Kohl <adeonir@gmail.com>
2023-09-24 23:34:25 -0300

    remove custom color from tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,6 @@ export default {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      colors: {
-        coffee: {
-          50: '#fcf8ea',
-          100: '#f8f0c9',
-          200: '#f2df96',
-          300: '#eac65a',
-          400: '#e2ae2d',
-          500: '#d59920',
-          600: '#b57519',
-          700: '#915417',
-          800: '#79441a',
-          900: '#67391c',
-          950: '#592b12',
-        },
-      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         coursive: ['Baloo 2', 'cursive'],
