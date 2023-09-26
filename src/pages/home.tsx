eefd10326d412e6dd640088d8c83cd112f887352
Adeonir Kohl <adeonir@gmail.com>
2023-09-26 17:49:10 -0300

    create coffee list component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
+import { CoffeeList } from './components/coffee-list'
 import { Hero } from './components/hero'
 
 export function Home() {
   return (
     <main>
       <Hero />
+      <CoffeeList />
     </main>
   )
 }
