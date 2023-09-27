8551ac7a53649a1cf37925ccf6282b0f255afe40
Adeonir Kohl <adeonir@gmail.com>
2023-09-27 15:07:09 -0300

    update coffee-list data and add cart-list data

---
+++ src/data/cart-list.ts
@@ -0,0 +1,17 @@
+export const cart = [
+  {
+    name: 'Expresso Tradicional',
+    price: '9,90',
+    image: 'src/assets/american.png',
+  },
+  {
+    name: 'Capuccino',
+    price: '11,40',
+    image: 'src/assets/capuccino.png',
+  },
+  {
+    name: 'Irlandês',
+    price: '15,90',
+    image: 'src/assets/irish.png',
+  },
+]
