575f64351cc4ddeef87f769bddab567d15d165de
Adeonir Kohl <adeonir@gmail.com>
2023-09-27 15:06:34 -0300

    update select styles

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -10,17 +10,17 @@ interface Props extends ComponentProps<'input'> {
 
 export function Select({ className, value, name, icon: Icon, ...props }: Props) {
   return (
-    <div className={className}>
+    <div className={cn('w-full', className)}>
       <input className="peer sr-only" id={value} name={name} type="radio" {...props} />
       <label
         htmlFor={value}
         className={cn(
           'inline-flex w-full cursor-pointer items-center justify-center gap-2 rounded-md p-4 uppercase transition',
-          'border-2 border-transparent bg-zinc-200 text-xs text-zinc-800 hover:bg-zinc-300',
+          'whitespace-nowrap border-2 border-transparent bg-zinc-200 text-xs text-zinc-800 hover:bg-zinc-300',
           'peer-checked:cursor-default peer-checked:border-indigo-500 peer-checked:bg-indigo-100',
         )}
       >
-        {Icon && <Icon className="h-4 w-4 text-indigo-700" />}
+        {Icon && <Icon className="h-4 w-4 shrink-0 text-indigo-700" />}
         <span className="h-3.5">{value}</span>
       </label>
     </div>
