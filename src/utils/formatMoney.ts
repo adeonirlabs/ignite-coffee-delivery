d6086a360280e1b8bf6d411760c4e81e4902611c
Adeonir Kohl <adeonir@gmail.com>
2023-09-29 09:14:47 -0300

    create format money util function

---
+++ src/utils/formatMoney.ts
@@ -0,0 +1,8 @@
+export function formatMoney(value: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  })
+    .format(value / 100)
+    .replace(/R\$\s/, '')
+}
