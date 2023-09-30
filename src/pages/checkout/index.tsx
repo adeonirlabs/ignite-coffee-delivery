18519dd98de285e7a3f9cb4a1f3641535fb2110e
Adeonir Kohl <adeonir@gmail.com>
2023-09-30 11:15:22 -0300

    update checkout page with new components and form submition

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,5 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Banknote, CreditCard, DollarSign, Landmark, MapPin } from 'lucide-react'
-import { useContext, useMemo } from 'react'
+import { useContext, useEffect, useMemo } from 'react'
+import { useForm } from 'react-hook-form'
 import { NavLink } from 'react-router-dom'
 
 import { Button } from '~/components/button'
@@ -7,19 +9,55 @@ import { CartItem } from '~/components/cart-item'
 import { Input } from '~/components/input'
 import { Select } from '~/components/select'
 import { CartContext } from '~/contexts/cart'
+import { cn } from '~/utils/classnames'
 import { formatMoney } from '~/utils/formatMoney'
 
+import { type CheckoutData, schema } from './schema'
+
 export function Checkout() {
-  const { cartItems } = useContext(CartContext)
+  const { cartItems, resetCart, submitCheckout } = useContext(CartContext)
 
   const deliveryValue = 500
   const itemsValue = useMemo(() => cartItems.reduce((acc, item) => acc + item.amount * item.price, 0), [cartItems])
   const totalValue = useMemo(() => (itemsValue ? itemsValue + deliveryValue : 0), [itemsValue])
 
+  const formMethods = useForm<CheckoutData>({
+    resolver: zodResolver(schema),
+  })
+
+  const {
+    formState: { errors, isSubmitting, isDirty },
+    handleSubmit,
+    register,
+    reset,
+    setError,
+    setValue,
+  } = formMethods
+
+  function handleSubmitForm(data: CheckoutData) {
+    submitCheckout(data)
+    resetCart()
+    reset()
+  }
+
+  useEffect(() => {
+    setValue('cartItems', cartItems)
+  }, [cartItems, setValue])
+
+  useEffect(() => {
+    if (cartItems.length === 0 && isDirty) {
+      setError('cartItems', { type: 'required' })
+    }
+  }, [cartItems.length, isDirty, setError])
+
   return (
     <section className="mx-auto max-w-6xl px-4 pb-24 pt-16">
       <h1 className="font-alt text-5xl font-black text-zinc-800">Checkout</h1>
-      <form className="mt-4 flex flex-col gap-8 lg:grid lg:grid-cols-[3fr_2fr]">
+      <form
+        className="mt-4 flex flex-col gap-8 lg:grid lg:grid-cols-[3fr_2fr]"
+        onSubmit={handleSubmit(handleSubmitForm)}
+        noValidate
+      >
         <div className="flex flex-col gap-4">
           <h2 className="font-alt text-2xl font-black text-zinc-800">Complete seu pedido</h2>
           <div className="rounded-ee-md rounded-es-4xl rounded-se-4xl rounded-ss-md bg-zinc-100 p-8">
@@ -31,16 +69,33 @@ export function Checkout() {
               </div>
             </div>
             <div className="mt-10 flex flex-col gap-4">
-              <Input label="CEP" id="cep" className="w-1/2 shrink-0 sm:w-1/4" required />
-              <Input label="Endereço" id="address" required />
+              <Input
+                {...register('zip', { valueAsNumber: true })}
+                error={!!errors?.zip}
+                className="w-1/2 shrink-0 sm:w-1/4"
+                placeholder="CEP"
+                type="number"
+              />
+              <Input {...register('address')} error={!!errors?.address} placeholder="Endereço" />
               <div className="flex flex-wrap gap-4 sm:flex-nowrap">
-                <Input label="Número" id="number" className="w-full shrink-0 sm:w-1/4" required />
-                <Input label="Complemento" id="complement" />
+                <Input
+                  {...register('number', { valueAsNumber: true })}
+                  error={!!errors?.number}
+                  className="w-full shrink-0 sm:w-1/4"
+                  placeholder="Número"
+                  type="number"
+                />
+                <Input {...register('complement')} placeholder="Complemento" required={false} />
               </div>
               <div className="flex flex-wrap gap-4 sm:flex-nowrap">
-                <Input label="Bairro" id="neighborhood" className="w-full shrink-0 sm:w-1/4" required />
-                <Input label="Cidade" id="city" required />
-                <Input label="Estado" id="state" className="w-3/12" required />
+                <Input
+                  {...register('neighborhood')}
+                  error={!!errors?.neighborhood}
+                  className="w-full shrink-0 sm:w-1/4"
+                  placeholder="Bairro"
+                />
+                <Input {...register('city')} error={!!errors?.city} placeholder="Cidade" />
+                <Input {...register('state')} error={!!errors?.state} className="w-3/12" placeholder="Estado" />
               </div>
             </div>
           </div>
@@ -52,10 +107,16 @@ export function Checkout() {
                 <p className="text-zinc-600">O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
               </div>
             </div>
-            <div className="mt-10 flex flex-col gap-4 sm:grid sm:grid-cols-3">
-              <Select name="payment" value="Cartão de crédito" icon={CreditCard} />
-              <Select name="payment" value="Cartão de débito" icon={Landmark} />
-              <Select name="payment" value="Dinheiro" icon={Banknote} />
+            <div className="mt-10">
+              <div
+                className={cn('flex flex-col gap-4 sm:grid sm:grid-cols-3', {
+                  'rounded-sm ring-2 ring-red-500 ring-offset-4 ring-offset-zinc-100': !!errors?.payment,
+                })}
+              >
+                <Select {...register('payment')} value="Cartão de crédito" icon={CreditCard} />
+                <Select {...register('payment')} value="Cartão de débito" icon={Landmark} />
+                <Select {...register('payment')} value="Dinheiro" icon={Banknote} />
+              </div>
             </div>
           </div>
         </div>
@@ -64,13 +125,19 @@ export function Checkout() {
           <div className="flex w-full flex-col gap-4 self-start rounded-ee-md rounded-es-4xl rounded-se-4xl rounded-ss-md bg-zinc-100 p-8">
             <div className="flex flex-col gap-6 divide-y-2 divide-zinc-200">
               {cartItems.length === 0 ? (
-                <p className="text-zinc-600">
-                  Nenhum item na lista,
-                  <NavLink className="px-1 font-medium text-amber-500 transition hover:text-amber-700" to="/">
-                    clique aqui
-                  </NavLink>
-                  para adicionar
-                </p>
+                <div
+                  className={cn({
+                    'rounded-sm ring-2 ring-red-500 ring-offset-4 ring-offset-zinc-100': !!errors?.cartItems,
+                  })}
+                >
+                  <p className="text-zinc-600">
+                    Nenhum item na lista,
+                    <NavLink className="px-1 font-medium text-amber-500 transition hover:text-amber-700" to="/">
+                      clique aqui
+                    </NavLink>
+                    para adicionar
+                  </p>
+                </div>
               ) : (
                 cartItems.map((item) => <CartItem key={item.name} className="pt-6 first:pt-0" coffee={item} />)
               )}
@@ -89,7 +156,7 @@ export function Checkout() {
                 </div>
               </div>
             </div>
-            <Button label="Finalizar Pedido" />
+            <Button type="submit" label="Finalizar Pedido" disabled={isSubmitting} />
           </div>
         </div>
       </form>
